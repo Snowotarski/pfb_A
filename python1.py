0763292e4b71755e0de0b9ddb185f3ae94e13f55
Nowotarski <stn@mp191tcl413.sgc.loc>
2019-08-20 11:05:00 -0500

    added sys.argv module to pull variables from command line input

--- python1.py
+++ python1.py
@@ -1,12 +1,11 @@
-#!/usr//bin/env python3 #call python environment automatically
+#!/usr/bin/env python3 #call python environment automatically
+import sys
 
-#variables
-name = 'steph'
-color = 'green'
-#activity = 'learning' 
+name = sys.argv[1] # get first command line parameter
+color = sys.argv[2] # get second command line parameter
+#activity = sys.argv[3]
+#animal = sys.argv [4] 
 
-#output
-print ('My name :'+ name)
-print ('My favorite color :' + color)
-
+#print a message to the screen
+print('My name is '+ name, 'and', color, ' is my favorite color.')
 
