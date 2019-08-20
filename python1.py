c26834a859a32d6bb366b32fd02b7950287ada39
Nowotarski <stn@mp191tcl413.sgc.loc>
2019-08-20 10:31:18 -0500

    added favorite color

--- python1.py
+++ python1.py
@@ -1,5 +1,12 @@
 #!/usr//bin/env python3 #call python environment automatically
 
-name = 'steph' 
+#variables
+name = 'steph'
+color = 'green'
+#activity = 'learning' 
+
+#output
 print ('My name :'+ name)
+print ('My favorite color :' + color)
+
 
