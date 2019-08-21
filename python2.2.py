ebb83d258c6ef4001c98bd650af2f03ccb0e2d2c
Nowotarski <stn@mp191tcl413.sgc.loc>
2019-08-21 11:52:43 -0500

    updated python2.2.py to increase logic with elif

--- python2.2.py
+++ python2.2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3 #call python environment automatically
 import sys
 
-# run chmod +x python2.2.py on command line to shortcut python 
+#run chmod +x python2.2.py on command line to shortcut python 
 #run ./python2.2.py inputnumber to run
 
 x = sys.argv[1] # get first command line parameter
@@ -9,14 +9,20 @@ x = sys.argv[1] # get first command line parameter
 
 y=float(x)	#convert any input to float
 
-if y > 0:
-	message = 'postive'
+if y < 0:
+	message = ' is a negative number'
 	print (message)
-elif y < 0:
-	message = 'negative'
-	print(message)
+elif y == 0:
+	message = ' is zero'
+	print(y,message)
+elif y > 50:
+	message = ' is greater than 50'
+	print (y, message)
+elif y < 50:
+	message= ' is less than 50'
+	print (y, message)
 else:
-	message = 'input was 0'
-	print(message)
+	message = ' is 50'
+	print(y, message)
 
 
